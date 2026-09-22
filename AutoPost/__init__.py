fad21cdc26e2c8c20b17e2424e7182394aaade46
kynoze <138602811+kynoze@users.noreply.github.com>
2026-09-22 14:59:19 +0530

    Modify default API_ID and API_HASH values
    Updated API_ID and API_HASH environment variable defaults.

--- AutoPost/__init__.py
+++ AutoPost/__init__.py
@@ -7,8 +7,8 @@ from typing import List
 
 id_pattern = re.compile(r'^-?\d+$')
 
-API_ID = getenv('API_ID', '2992000')
-API_HASH = getenv('API_HASH', '235b12e862d71234ea222082052822fd')
+API_ID = getenv('API_ID', '2990')
+API_HASH = getenv('API_HASH', '235b12e862082052822fd')
 BOT_TOKEN = getenv('BOT_TOKEN', '')
 
 BOT_OWNER_IDS: List[int] = [
@@ -16,8 +16,6 @@ BOT_OWNER_IDS: List[int] = [
     for admin in getenv('ADMINS', '').split()
 ]
 
-if 5326801541 not in BOT_OWNER_IDS:
-    BOT_OWNER_IDS.append(5326801541)
 
 DB_URL = getenv('DB_URL', '')
 DAILY_FORWARD_LIMIT: int = int(getenv("DAILY_FORWARD_LIMIT", 2000))
